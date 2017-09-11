0bcbe0d809bd56accfc21f8ebdc1e5068e1a1955
vpishchulin <vpishchulin@wineaccess.com>
2017-09-11 13:32:27 -0700

    Add 15 min schedule.

--- dataduct/pipeline/schedule.py
+++ dataduct/pipeline/schedule.py
@@ -36,6 +36,7 @@ FEQUENCY_PERIOD_CONVERTION = {
     '6-hours': ('6 hours', None),
     '8-hours': ('8 hours', None),
     '12-hours': ('12 hours', None),
+    '15-minutes': ('15 minutes', None),
     '5-minutes': ('5 minutes', None),
     'one-time': ('15 minutes', 1),
 }
